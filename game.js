b9e1c1e75190b3aba93f1191fec4ca89f9885df9
Your Name <yourname@example.com>
2024-10-31 13:54:27 +0800

    added event listeners to the game board

--- game.js
+++ game.js
@@ -13,6 +13,18 @@ form.addEventListener('submit', (event) => {
 
 
 
+// initialize variables
+const initializeVariables = (data) => {
+    data.board = [0,1,2,3,4,5,6,7,8]
+    data.player1 = "X";
+    data.player2 = "O";
+    data.round = 0;
+    data.currentPlayer = "X";
+    data.gameOver = false;
+}
+
+
+
 // atttach event listeners to each gamebox
 const addEventListenerToGameBoard = (data) => {
     document.querySelectorAll('.box').forEach(box => {
@@ -24,17 +36,7 @@ const addEventListenerToGameBoard = (data) => {
 
 
 
-
 // initialize the game
-const initializeVariables = (data) => {
-    data.board = [0,1,2,3,4,5,6,7,8]
-    data.player1 = "X";
-    data.player2 = "O";
-    data.round = 0;
-    data.currentPlayer = "X";
-    data.gameOver = false;
-}
-
 const initializeGame = (data) => {
     // initialize game variable
     initializeVariables(data);
@@ -43,6 +45,27 @@ const initializeGame = (data) => {
     addEventListenerToGameBoard(data);
 }
 
+const playMove = (box,data) => {
+    // is game over? if game over, dont do anything
+    if(data.gameOver || data.round > 8) {
+        return;
+    }
+    // check if game box has a letter in it, if so, dont do anythings
+    if(data.board[box.id] === "X" || data.board[box.id] === "O") {
+        return;
+    }
+
+    // adjust the DOM for player move, and then check win conditions
+    data.board[box.id] = data.currentPlayer
+    box.textContent = data.currentPlayer
+    box.classList.add(data.currentPlayer === "X" ? "player1" : "player2")
+
+    // increase the round #
+    data.round++;
+    console.log(box,data)   
+
+    // check win conditions
+}
 
 
 // need to determine current player
@@ -51,15 +74,7 @@ const initializeGame = (data) => {
 
 // set win conditions for the game
 
-const playMove = (box,data) => {
-    // is game over? if game over, dont do anything
-    if(data.gameOver) {
-        return
-    }
-    // check if game box has a letter in it
 
-    console.log(box,data)   
-}
 
 
 
