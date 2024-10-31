da6de8b3961354c8e2e971be997f00ff48120bd1
Your Name <yourname@example.com>
2024-10-31 13:19:38 +0800

    Arranged the data for easy understanding

--- game.js
+++ game.js
@@ -19,5 +19,22 @@ form.addEventListener('submit', (event) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
     document.querySelector(".modal-wrapper").setAttribute('hidden', true)
-    console.log(data)
+    initializeGame(data);
 })
+
+const initializeVariables = (data) => {
+    data.board = [0,1,2,3,4,5,6,7,8]
+    data.player1 = "X";
+    data.player2 = "O";
+    data.round = 0;
+    data.currentPlayer = "X";
+    data.gameOver = false;
+}
+
+const initializeGame = (data) => {
+    // initialize game variable
+    initializeVariables(data);
+
+    console.log(data);
+    // add event listeners to the gameboard
+}
